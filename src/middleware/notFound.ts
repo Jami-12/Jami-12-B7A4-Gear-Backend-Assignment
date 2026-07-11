92b478ff94d2588cc05ce6df03de6d0029a11885
Mujaddid Ahmed Jami <mujaddidahmedjami2025@gmail.com>
2026-07-11 21:53:55 +0600

    Add payment confirmation and tracking

--- src/middleware/notFound.ts
+++ src/middleware/notFound.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 export const notFound = (req: Request, res: Response) => {
   res.status(404).json({
-    message: "Route not found",
+    message: "Route not ",
     path: req.originalUrl,
     date: new Date(),
   });
